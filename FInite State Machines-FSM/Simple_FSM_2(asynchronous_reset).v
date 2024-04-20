72abcab69ef165e7bb0e3d7757af289cf40bf741
anik-m <131432533+anik-m@users.noreply.github.com>
2024-04-21 04:02:22 +0600

    FSM reference. Just add it

---
+++ FInite State Machines-FSM/Simple_FSM_2(asynchronous_reset).v
@@ -0,0 +1,32 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        if(state == OFF)
+            next_state <= (j)?ON:OFF;
+        else if(state == ON)
+            next_state <= (k)?OFF:ON;
+            
+    end
+
+    always @(posedge clk, posedge areset) begin
+        // State flip-flops with asynchronous reset
+        if(areset)
+            state<=OFF;
+        else 
+            state<=next_state;
+    end
+
+    assign out = state;
+    // Output logic
+    // assign out = (state == ...);
+
+endmodule
